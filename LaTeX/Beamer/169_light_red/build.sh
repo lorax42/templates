c09b1cad2bb5710b7219aed09ac3ee742c33b0f7
lorax <142108266+lorax42@users.noreply.github.com>
2024-03-17 11:44:53 +0100

    add 169_light_red beamer template

---
+++ LaTeX/Beamer/169_light_red/build.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+
+if [[ "$1" -eq "1" ]]
+then
+    xdg-open main.pdf
+elif [[ "$1" -eq "2" ]]
+then
+    pdflatex main
+    xdg-open main.pdf
+else
+    pdflatex main
+    xdg-open main.pdf
+fi
+
